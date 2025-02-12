e2fd7124b0359a7c0ca6e95d3680801eeadfd09e
sampano <samuelpano@gmail.com>
2025-02-12 21:38:29 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  function signUp(formData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log(email);
+    console.log(password);
+  }
+
+  return (
+    <section>
+      <h1>Signup form</h1>
+      <form action={signUp}>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="joe@schmoe.com"
+        />
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" />
+        <br />
+
+        <button>Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default App;
